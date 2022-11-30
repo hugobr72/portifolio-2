3c3f99c1bf1182bb7c962fe99794a3b5458a451c
hugobr72 <hugobrod72@gmail.com>
2022-11-29 21:36:12 -0300

    instagrem error

--- src/components/Section.js
+++ src/components/Section.js
@@ -24,7 +24,7 @@ const Section = ({ sobre }) => {
                         foi uma das melhores escolhas que fiz em minha vida.
                     </p>
                     <br />
-                    <h3><a href="https://www.instagram.com/hugobrod_/">Intagram</a> <a href="https://github.com/hugobr72">Github</a>
+                    <h3><a href="https://www.instagram.com/hugobrod_/">Instagram</a> <a href="https://github.com/hugobr72">Github</a>
                         <a href="https://api.whatsapp.com/send?phone=5535997164602&text=Ol%C3%A1%2C%20Seja%20bem%20vindo!">Whatsapp</a></h3>
                     <h3 id='btn-cv'><a href={CV} download={CV}>Baixar CV</a></h3>
                 </div>
